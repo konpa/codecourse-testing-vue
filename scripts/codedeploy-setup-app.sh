df9d3fdab5f27c82233e6edef7c2f3711674eef8
Julien Monty <julien.monty@gmail.com>
2021-06-16 08:36:47 +0200

    Update AWS scripts

--- scripts/codedeploy-setup-app.sh
+++ scripts/codedeploy-setup-app.sh
@@ -1,15 +1,13 @@
-mv /var/www/learn-app/shared/.env /var/www/learn-app/html/.env
+cp /var/www/learn-app/shared/.env /var/www/learn-app/html/.env
 
 cd /var/www/learn-app/html
 
 find ./ -type f -exec chmod 664 {} \;
 find ./ -type d -exec chmod 775 {} \;
+chown -R admin:www-data /var/www/learn-app/html
 chgrp -R www-data ./storage ./bootstrap/cache
 chmod -R ug+rwx ./storage ./bootstrap/cache
 
 composer install --prefer-dist --no-scripts -q -o
-
-php artisan config:cache
-php artisan route:cache
 
 touch /tmp/deployment-done
